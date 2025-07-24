5316952ff0ae166d60e2d92cc7d33ec63aa2a834
IlTuoNome <laTuaEmail@example.com>
2025-07-24 18:27:17 +0200

    Resolve Snack 1

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+// Snack 1
+
+let dato: unknown;
+if(typeof dato === 'string') {
+  console.log(dato.toUpperCase());
+}else if(typeof dato === 'number') {
+  console.log(dato.toFixed(2));
+}else if(typeof dato === 'boolean') {
+  console.log(dato ? 'E vero' : 'E falso');
+}else {
+  console.log('Tipo non supportato');
+}
